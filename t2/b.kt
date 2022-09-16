b14005f99ffa547a38ead4c405a93c199752ee0f
vadniks <hidden@some.a>
2022-09-16 11:35:30 +0300

    Added tasks 1-2

--- t2/b.kt
+++ t2/b.kt
@@ -10,16 +10,12 @@ import kotlin.math.sqrt
 interface a : Remote
 { @Throws(RemoteException::class) fun a(a: Float, b: Float, c: Float): Pair<Float, Float?> }
 
-class b : a {
-    override fun a(a: Float, b: Float, c: Float): Pair<Float, Float?> {
-        val d = a * a - 4 * a * c
-        return (-b + sqrt(d)) / (2 * a) to if (d > 0) (-b - sqrt(d)) / (2 * a) else null
-    }
-}
+class b : a { override fun a(a: Float, b: Float, c: Float): Pair<Float, Float?> {
+    val d = a * a - 4 * a * c
+    return (-b + sqrt(d)) / (2 * a) to if (d > 0) (-b - sqrt(d)) / (2 * a) else null
+} }
 
-fun main() {
-    LocateRegistry.createRegistry(1099).bind(
-        a::class.simpleName,
-        UnicastRemoteObject.exportObject(b(), 0)
-    )
-}
+fun main() = LocateRegistry.createRegistry(1099).bind(
+    a::class.simpleName,
+    UnicastRemoteObject.exportObject(b(), 0)
+)
